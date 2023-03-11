aec7a1c718ecfad3a2ead316875a7490011ac949
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-03-11 22:41:58 +0300

    stock index in google sheet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.google.com/spreadsheets/d/1ONEl1Xi5GwR-hyFjSi0dasfl02DY_OV6_HOGhQ2Xau0/edit#gid=0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out google Stock Prices in googlesheet
         </a>
       </header>
     </div>
